ba37e21a34ec438db416e6c1012288cb2d22c6ae
AlMamunTalukder <almamuntalukder0@gmail.com>
2026-06-17 01:55:07 +0600

    update the admission and broken website pblm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={` ${siliguri.className} antialiased`}>
-        {/* <SubHeader /> */}
+
         <Header />
         {children}
         <Toaster />
